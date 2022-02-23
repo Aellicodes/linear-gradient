e99e936252edf75d9a5d71e2fdf8f035e4588db4
Aelli codes <code.essence.mail@gmail.com>
2022-02-23 18:32:26 +0100

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent implements AfterViewInit{
 
   public chartLabels: Label[] = ['El1', 'El2', 'El3', 'El4'];
   public chartData: ChartDataSets[] = [{data: [26,	21, 35, 42], label: 'Data 1',
-  fill: true}];
+fill: true}];
 
   public chartOptions: ChartOptions = {
     responsive: true,
@@ -33,26 +33,23 @@ export class AppComponent implements AfterViewInit{
   }];
 
   ngAfterViewInit(): void {
-    const ctx = this.canvas.nativeElement.getContext('2d');
-    const gradientStroke = ctx.createLinearGradient(500, 0, 100, 0);
-    gradientStroke.addColorStop(0, colors.darkColor);
-    gradientStroke.addColorStop(0.3, colors.lightColor);
-    gradientStroke.addColorStop(0.6, colors.chartColor1);
-    gradientStroke.addColorStop(1, colors.chartColor2);
+      const ctx = this.canvas.nativeElement.getContext('2d');
+      const gradientStroke = ctx.createLinearGradient(100, 0, 500, 0);
+      gradientStroke.addColorStop(0, colors.darkColor);
+      gradientStroke.addColorStop(0.3, colors.lightColor);
+      gradientStroke.addColorStop(0.6, colors.chartColor1);
+      gradientStroke.addColorStop(1, colors.chartColor2);
 
-    const gradientFill = ctx.createLinearGradient(500, 0, 100, 0);
-    gradientFill.addColorStop(0, "rgba(100, 201, 207, 0.6)");
-    gradientFill.addColorStop(0.3, "rgba(253, 228, 156, 0.6)");
-    gradientFill.addColorStop(0.6, "rgba(255, 183, 64, 0.6)");
-    gradientFill.addColorStop(1, "rgba(223, 113, 27, 0.6)");
+      const gradientFill = ctx.createLinearGradient(100, 0, 500, 0);
+      gradientFill.addColorStop(0, "rgba(100, 201, 207, 0.6)");
+      gradientFill.addColorStop(0.3, "rgba(253, 228, 156, 0.6)");
+      gradientFill.addColorStop(0.6, "rgba(255, 183, 64, 0.6)");
+      gradientFill.addColorStop(1, "rgba(223, 113, 27, 0.6)");
 
-    this.chartColors = [
-      {
+      this.chartColors = [{
         borderColor: gradientStroke,
-        pointBorderColor: gradientStroke,
         pointBackgroundColor: gradientStroke,
         backgroundColor: gradientFill
-      }
-    ];
+      }];
   }
 }
